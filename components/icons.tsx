e25a338ce9f246dfd6b34461e60de49106275b30
Michael Madry <michael_madry@hotmail.com>
2023-06-19 00:46:23 +1000

    init: edit and delete rulesets

--- components/icons.tsx
+++ components/icons.tsx
@@ -3,6 +3,8 @@ import {
   Moon,
   SunMedium,
   Twitter,
+  Pencil,
+  XCircle,
   type Icon as LucideIcon,
 } from "lucide-react"
 
@@ -12,6 +14,8 @@ export const Icons = {
   sun: SunMedium,
   moon: Moon,
   twitter: Twitter,
+  pencil: Pencil,
+  xcircle: XCircle,
   logo: (props: LucideProps) => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
       <path
